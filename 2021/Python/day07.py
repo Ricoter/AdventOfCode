0d77d06c5e8f35228b2321065f078b2d10de66cc
Ricoter <rico.vanmidde@gmail.com>
2021-12-07 17:08:50 +0100

    numpy cleanup

--- 2021/Python/day07.py
+++ 2021/Python/day07.py
@@ -5,26 +5,21 @@ def readData(infile):
         data = np.array(f.readline().split(',')).astype(int)
     return data
 
-def part1(data):
-    fuelCosts = np.zeros(max(data))
-    positions = np.arange(max(data))
-    for x in data:
-        dx = np.abs(positions - x)
-        fuelCosts += dx
-    return  int(fuelCosts.min())
+def linear(x):
+    return x
 
-def cost(dx):
+def stepwiseIncremental(dx):
     return dx*(dx+1)/2
 
-def part2(data):
+def solve(data, costFn):
     fuelCosts = np.zeros(max(data))
     positions = np.arange(max(data))
     for x in data:
         dx = np.abs(positions - x)
-        fuelCosts += cost(dx)
+        fuelCosts += costFn(dx)
     return  int(fuelCosts.min())
 
 if __name__=='__main__':
     data = readData('../Data/day07')
-    print(part1(data))
-    print(part2(data))
+    print(solve(data, costFn=linear))
+    print(solve(data, costFn=stepwiseIncremental))
