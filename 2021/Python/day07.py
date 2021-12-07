6168f3d673db47481516c81dd8e622a36d18695a
Ricoter <rico.vanmidde@gmail.com>
2021-12-07 14:50:53 +0100

    day7

--- 2021/Python/day07.py
+++ 2021/Python/day07.py
@@ -2,18 +2,26 @@ import numpy as np
 
 def readData(infile):
     with open(infile, 'r') as f:
-        data = [line.strip('\n') for line in f]
-    # data = [line.split(' ') for line in data]
-    # data = np.array([list(line) for line in data]).astype(int)
+        data = np.array(f.readline().split(',')).astype(int)
     return data
 
 def part1(data):
-    return
+    fuelCosts = np.zeros(max(data))
+    positions = np.arange(max(data))
+    for x in data:
+        dx = np.abs(positions - x)
+        fuelCosts += dx
+    return  int(fuelCosts.min())
 
 def part2(data):
-    return 
+    fuelCosts = np.zeros(max(data))
+    positions = np.arange(max(data))
+    for x in data:
+        dx = np.abs(positions - x)
+        fuelCosts += dx*(dx+1)/2
+    return  int(fuelCosts.min())
 
 if __name__=='__main__':
-    data = readData('../Data/day03')
+    data = readData('../Data/day07')
     print(part1(data))
     print(part2(data))
