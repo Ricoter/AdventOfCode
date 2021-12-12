7ca031cbec04baa959c642306d771d2b09876a1d
Ricoter <rico.vanmidde@gmail.com>
2021-12-12 02:35:38 +0100

    day 11

--- 2021/Python/day11.py
+++ 2021/Python/day11.py
@@ -1,19 +1,46 @@
 import numpy as np
+from scipy import ndimage
 
 def readData(infile):
     with open(infile, 'r') as f:
         data = [line.strip('\n') for line in f]
-    # data = [line.split(' ') for line in data]
-    # data = np.array([list(line) for line in data]).astype(int)
+    data = np.array([list(line) for line in data]).astype(float)
+    data = np.pad(data, pad_width=1, mode='constant', constant_values=-np.inf)
     return data
 
+
 def part1(data):
-    return
+    total = 0
+    for _ in range(100):
+        data += 1
+        flash = list(zip(*np.where(data==10)))
+        for i, j in flash:
+            for di in range(i-1, i+2):
+                for dj in range(j-1, j+2):
+                    data[di, dj] += 1
+                    if data[di, dj] == 10:
+                        flash.append((di, dj))
+        total += len(flash)
+        data[data > 9] = 0
+    return int(total)
+
 
 def part2(data):
-    return 
+    for run in range(1000):
+        data += 1
+        flash = list(zip(*np.where(data==10)))
+        for i, j in flash:
+            for di in range(i-1, i+2):
+                for dj in range(j-1, j+2):
+                    data[di, dj] += 1
+                    if data[di, dj] == 10:
+                        flash.append((di, dj))
+        data[data > 9] = 0 
+        if np.sum(data==0) == 100:
+            return run+1
+    return False
 
 if __name__=='__main__':
-    data = readData('../Data/day03')
-    print(part1(data))
+    data = readData('../Data/day11')
+    print(part1(np.copy(data)))
     print(part2(data))
