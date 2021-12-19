4ee33f7cc6ab472a969a2a2b5045e47836e40edc
Ricoter <rico.vanmidde@gmail.com>
2021-12-19 17:37:08 +0100

    template

---
+++ 2021/Python/template.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def readData(infile):
+    with open(infile, 'r') as f:
+        data = [line.strip('\n') for line in f]
+    # data = [line.split(' ') for line in data]
+    # data = np.array([list(line) for line in data]).astype(int)
+    return data
+
+def part1(data):
+    return
+
+def part2(data):
+    return 
+
+if __name__=='__main__':
+    data = readData('../Data/day13')
+    print(part1(np.copy(data)))
+    print(part2(data))
